8654aa5b86f40a1a15d445e111a491b6b36423a9
unoof <bobbyvn6@gmail.com>
2025-03-04 20:03:44 +0700

    Add files via upload

---
+++ CHELLO.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void submit()
+{
+
+    
+    cout << "Hello PTIT.";
+
+    cout << endl;
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);  // Speed up input/output
+    cin.tie(nullptr);
+
+    submit();
+
+    return 0;
+}
