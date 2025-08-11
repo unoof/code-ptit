9386cc31a6fcec2afb41f4d58569a44c70b75dd2
unoof <bobbyvn6@gmail.com>
2025-08-11 20:15:15 +0700

    Create CHELLO Hello World.cpp

---
+++ DSA/CHELLO Hello World.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void submit()
+{
+    cout << "Hello PTIT.";
+}   
+
+int main()
+{
+    ios::sync_with_stdio(false);                                    // Speed up input/output
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    submit();
+
+    return 0;
+}
